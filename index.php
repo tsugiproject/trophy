303dcf5f87178fa075a79b09446e9d89bd25d383
Charles Severance <csev@umich.edu>
2019-11-16 23:35:46 -0500

    Add the dump

--- index.php
+++ index.php
@@ -14,10 +14,16 @@ $OUTPUT->topNav();
 
 $OUTPUT->welcomeUserCourse();
 
-if ( ! $LTI->user->instructor ) {
+if ( $LTI->user->instructor ) {
+    echo("<p>Instructors can't send grades with LTI so here is a LAUNCH dump</p>\n");
+    echo("<pre>\n");var_dump($LTI);echo("</pre>\n");
+    echo("<p>All the 'cooked' LTI parameters</p>\n");
+    echo("<pre>\n");var_dump($LTI->ltiParameterArray());echo("</pre>\n");
+} else {
    echo('<center><i class="fa fa-trophy fa-5x" style="color: blue;"></i>');
    echo('<br/>You earned a trophy!</center>');
    $LTI->result->gradeSend(0.95, false);
 }
+
 
 $OUTPUT->footer();
