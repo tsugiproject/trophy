143ecd531ccd54cc6162d7ab0e02f4b409eb508d
Charles Severance <csev@umich.edu>
2021-08-06 16:44:38 -0400

    Challenge test code

--- index.php
+++ index.php
@@ -14,6 +14,8 @@ $OUTPUT->topNav();
 
 $OUTPUT->welcomeUserCourse();
 
+$origin = $LTI->ltiJWTClaim("https://purl.imsglobal.org/spec/lti/claim/origin");
+
 if ( $LTI->user->instructor ) {
     echo("<p>Instructors can't send grades with LTI so here is a LAUNCH dump</p>\n");
     echo("<pre>\n");var_dump($LTI);echo("</pre>\n");
@@ -30,6 +32,44 @@ if ( $LTI->user->instructor ) {
         echo('And your grade was stored locally<br>');
     }
     echo("\n</center>\n");
+}
+
+if ( $origin ) {
+$postjson = new \stdClass();
+$postjson->subject = "org.imsglobal.lti.put_data";
+$postjson->key = 'answer';
+$postjson->value = 42.0;
+$postjson->message_id = 42;
+$postset = json_encode($postjson);
+
+$postjson = new \stdClass();
+$postjson->subject = "org.imsglobal.lti.get_data";
+$postjson->key = 'answer';
+$postjson->message_id = 42;
+$postget = json_encode($postjson);
+
+$postjson->subject = "org.imsglobal.lti.get_data";
+$postjson->key = 'state';
+$postjson->message_id = 42;
+$poststate = json_encode($postjson);
+?>
+<script>
+window.addEventListener('message', function (e) {
+    console.log('Trophy received message');
+    console.log(e.data);
+    console.log((e.source == parent ? 'Source parent' : 'Source not parent '+e.source), '/', 
+                (e.origin == '<?= $origin ?>' ? 'Origin match' : 'Origin mismatch '+e.origin));
+});
+console.log('trophy sending org.imsglobal.lti.put_data');
+parent.postMessage(<?= $postset ?>, '<?= $origin ?>');
+console.log('Do I need a setTimeout here??');
+console.log('trophy sending org.imsglobal.lti.get_data');
+parent.postMessage(<?= $postget ?>, '<?= $origin ?>');
+console.log('trophy sending reading state');
+parent.postMessage(<?= $poststate ?>, '<?= $origin ?>');
+
+</script>
+<?php
 }
 
 
